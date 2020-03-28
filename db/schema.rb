ce8e03f43f24817a5b89fe703dfaab6bca154f58
Ubuntu <ubuntu@ip-172-31-45-141.ap-northeast-2.compute.internal>
2020-03-28 06:53:54 +0000

    go master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_25_113942) do
+ActiveRecord::Schema.define(version: 2019_07_09_085348) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
