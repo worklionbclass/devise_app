de6d5e9e0080398a08986fd29e6fea07dcfb0ebf
Ubuntu <ubuntu@ip-172-31-45-141.ap-northeast-2.compute.internal>
2020-03-28 07:28:37 +0000

    code 수정

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     
     @post.title = params[:title]
     @post.content = params[:content]
-    @post.user_email = params[:email]
+    @post.user_id = current_user.id
     
     @post.save
     
